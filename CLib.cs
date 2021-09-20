b033acb5b2d7002fc83a7704ec271a71da9edb87
John Connor <jckusho@gmail.com>
2021-09-20 17:11:13 -0400

    Added support for finding player by SteamID

--- CLib.cs
+++ CLib.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using Sandbox;
+// using Steamworks;
 
 namespace Custodia
 {
@@ -11,7 +12,12 @@ namespace Custodia
 
 		private static DateTime DT { get; } = new(1970, 1, 1, 0, 0, 0);
 
-		public static Client FindPlayerFromText(string words) =>
-			Client.All.FirstOrDefault(c => c.Name.Contains(words));
+		public static Client FindPlayerFromText(string words)
+		{
+			if (ulong.TryParse(words, out ulong provided))
+				return Client.All.FirstOrDefault(c => c.SteamId == provided);
+			else
+				return Client.All.FirstOrDefault(c => c.Name.Contains(words));
+		}
 	}
 }
